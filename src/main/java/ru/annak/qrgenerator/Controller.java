93afa64a86e80e7de8e7681e46d8e39aa99f471e
alexey-va1 <freedeeml@gmail.com>
2024-05-28 14:23:35 +0300

    fix

--- src/main/java/ru/annak/qrgenerator/Controller.java
+++ src/main/java/ru/annak/qrgenerator/Controller.java
@@ -1,4 +1,4 @@
-package ru.alexeyva.qrgenerator;
+package ru.annak.qrgenerator;
 
 import lombok.SneakyThrows;
 import net.fortuna.ical4j.model.*;
@@ -18,11 +18,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.File;
-import java.io.FileWriter;
 import java.nio.file.Files;
-import java.sql.Date;
 import java.time.LocalDate;
-import java.time.LocalDateTime;
 import java.util.concurrent.ThreadLocalRandom;
 
 @RestController
@@ -34,7 +31,9 @@ public class Controller {
                                         @RequestParam(name = "duration", required = false) Integer daysDuration,
                                         @RequestParam(name = "step", required = false) Integer repeatEvery,
                                         @RequestParam(name = "timezone", required = false) String tz,
-                                        @RequestParam(name = "summary", required = false) String summary){
+                                        @RequestParam(name = "summary", required = false) String summary) {
+
+        summary = "Пора принимать БАДы EasyLink";
 
         TimeZoneRegistry registry = TimeZoneRegistryFactory.getInstance().createRegistry();
         TimeZone timezone = tz == null ? registry.getTimeZone("Europe/Moscow") : registry.getTimeZone(tz);
@@ -56,20 +55,20 @@ public class Controller {
 
         Recur recur = new Recur(Recur.DAILY, duration);
         recur.setInterval(step);
-        System.out.println(duration+" "+step);
+        System.out.println(duration + " " + step);
 
         RRule rule = new RRule(recur);
         event.add(rule);
 
         Calendar calendar = new Calendar();
-        calendar.add(new ProdId("-//Alexey//QR//EN"));
+        calendar.add(new ProdId("-//Anna//QR//EN"));
         calendar.add(ImmutableVersion.VERSION_2_0);
         calendar.add(ImmutableCalScale.GREGORIAN);
 
         calendar.add(event);
         calendar.add(vTimeZone);
 
-        File tempFile = File.createTempFile("pills"+ ThreadLocalRandom.current().nextInt(0,1000), ".ics");
+        File tempFile = File.createTempFile("pills" + ThreadLocalRandom.current().nextInt(0, 1000), ".ics");
         tempFile.deleteOnExit();
 
         Files.writeString(tempFile.toPath(), calendar.toString());
